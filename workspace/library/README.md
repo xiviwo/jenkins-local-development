0b0888c49cbb82398d63e022c0643858747930a3
Xiong Chang Wu <xiviwo@gmail.com>
2022-03-26 20:04:06 +0800

    test

--- workspace/library/README.md
+++
@@ -1,6 +0,0 @@
-# jenkins-shared-library-framework
-
-A Community Framework for CI/CD using Jenkins shared library
-
-You can clone this repo and start using it in your Jenkins Pipelines.
-
--- workspace/library/vars/gitCheckout.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map stageParams) {
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
